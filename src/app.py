6c800a4ae5e142882b22d21aaa196daeed7a6e77
darioduarte1 <darioduarte1991@gmail.com>
2024-07-30 14:28:55 +0000

    Build a StarWars REST API

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Spaceships
 #from models import Person
 
 app = Flask(__name__)
@@ -31,19 +31,51 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# Ruta para GET de todos los people en la Base de Dados
+@app.route('/people', methods=['GET'])
+def get_people():
+    characters_query = Characters.query.all()
+    characters_text = [character.serialize() for character in characters_query]
+    return jsonify (characters_text), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# Ruta para GET de un character especifico en la Base de Dados
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_character(people_id):
+    characters_query = Characters.query.filter_by(id=people_id).first()
+    if characters_query:
+        response_body = {
+            "msg": "Character encontrado",
+            "result": characters_query.serialize()
+        }
+        return jsonify (response_body), 200
+    else:
+        return jsonify ({"message": "Character no encontrado"}), 404
 
-    return jsonify(response_body), 200
+# Ruta para GET de todos los planetas en la Base de Dados
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets_query = Planets.query.all()
+    planets_text = [planet.serialize() for planet in planets_query]
+    return jsonify (planets_text), 200
+
+# Ruta para GET de un planeta especifico en la Base de Dados
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet_query = Planets.query.filter_by(id=planet_id).first()
+    if planet_query:
+        response_body = {
+            "msg": "Planet encontrado",
+            "result": planet_query.serialize()
+        }
+        return jsonify (response_body), 200
+    else:
+        return jsonify ({"message": "Planet no encontrado"}), 404
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,119 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=True)
+    first_name = Column(String(250), nullable=True)
+    last_name = Column(String(250), nullable=True)
+    email = Column(String(250), nullable=True)
+    password = Column(String(250), nullable=True)
+    favorite = relationship("Favorites", backref="user")
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return '<User %r>' % self.email
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    race = Column(String(250), nullable=True)
+    height = Column(String(250), nullable=True)
+    eyes_color = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "race": self.race,
+            "height": self.height,
+            "eyesColor": self.eyes_color,
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    local = Column(String(250), nullable=True)
+    dimension = Column(String(250), nullable=True)
+    color = Column(String(250), nullable=True)
+    favorite = relationship("Favorites", backref="planets")
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "local": self.local,
+            "dimension": self.dimension,
+            "color": self.color,
+            # do not serialize the password, its a security breach
+        }
+
+class Spaceships(db.Model):
+    __tablename__ = 'spaceships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    color = Column(String(250), nullable=True)
+    guns = Column(String(250), nullable=True)
+    velocity = Column(String(250), nullable=True)
+    favorite = relationship("Favorites", backref="spaceships")
+
+    def __repr__(self):
+        return '<Spaceships %r>' % self.name
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "color": self.color,
+            "guns": self.guns,
+            "velocity": self.velocity,
+            # do not serialize the password, its a security breach
+        }
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_fk = Column(Integer, ForeignKey("user.id"))
+    characters_fk = Column(Integer, ForeignKey("characters.id"))
+    planet_fk = Column(Integer, ForeignKey("planets.id"))
+    spaceships_fk = Column(Integer, ForeignKey("spaceships.id"))
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+    def serialize(self):
+        return {
+            "id": self.id,
+            "userFK": self.user_fk,
+            "charactersFK": self.characters_fk,
+            "PlanetFK": self.planet_fk,
+            "spaceshipsFK": self.spaceships_fk,
+            # do not serialize the password, its a security breach
+        }
+    
+
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
